55482975299b9b483e2f452233e60d73fa1c1bdf
i_mas_d <imasdsiainteractive@gmail.com>
2025-05-15 20:07:37 -0300

    upgrade vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,14 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  root: ".", 
   build: {
+    outDir: "dist",
     rollupOptions: {
-      input: "index.html",
-      external: []
+      input: "/src/main.jsx", // Cambiado a .jsx
     },
   },
-  resolve: {
-    alias: {
-      '@': '/src'
-    }
-  }
 });
