a24baddf14347787398cb3f45d27cd53b1d93310
elgitdemarce <124519731+elgitdemarce@users.noreply.github.com>
2025-05-15 19:34:23 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: ".",
   build: {
-    outDir: "dist"
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: "index.html"
+      }
+    }
   }
 });
