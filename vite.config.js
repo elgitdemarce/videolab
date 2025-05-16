7cfcd61407cb746f32f3e9ee32ebbb6c65705d40
i_mas_d <imasdsiainteractive@gmail.com>
2025-05-16 08:50:07 -0300

    d

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: 'dist',
     rollupOptions: {
       external: ['@ffmpeg/ffmpeg']
     }
-  }
+  },
+  base: '/'
 });
