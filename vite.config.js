a7c294d4524176e2d7145f64590fd3dbef0a9ab4
i_mas_d <imasdsiainteractive@gmail.com>
2025-05-15 21:09:03 -0300

    bbb

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,11 @@
-// vite.config.js
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: ".", 
   build: {
-    outDir: "dist",
     rollupOptions: {
-      input: "/src/main.jsx", // Cambiado a .jsx
-    },
-  },
+      external: ['@ffmpeg/ffmpeg']
+    }
+  }
 });
